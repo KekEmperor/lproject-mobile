5e51a28363f76b44a5841199d4c32e63ace61403
Rostyslav Tukalo <tukalo20010825@gmail.com>
2020-12-18 12:23:44 +0200

    last

--- app/src/main/java/com/example/lproject_mobile/ListEventActivity.kt
+++ app/src/main/java/com/example/lproject_mobile/ListEventActivity.kt
@@ -3,13 +3,8 @@ package com.example.lproject_mobile
 import android.content.Context
 import android.os.Build
 import android.os.Bundle
-import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.marginTop
-import kotlinx.android.synthetic.main.activity_catalog_event.*
 import kotlinx.android.synthetic.main.activity_catalog_event.addressTextView
 import kotlinx.android.synthetic.main.activity_catalog_event.nameTextView
 import kotlinx.android.synthetic.main.activity_catalog_event.startDateTextView
@@ -20,8 +15,13 @@ import kotlinx.coroutines.launch
 import org.json.JSONObject
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
+import android.nfc.NfcAdapter
+import android.util.Log
+import android.widget.*
 
 class ListEventActivity : AppCompatActivity() {
+    private lateinit var nfcAdapter: NfcAdapter
+
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -63,20 +63,77 @@ class ListEventActivity : AppCompatActivity() {
                     runOnUiThread {
                         val tv = TextView(applicationContext)
                         tv.textSize = 20f
-                        tv.text = location.getString("name") + ": " + ZonedDateTime.parse(item.getString("startTime"))
-                            .format(DateTimeFormatter.ofPattern("dd.MM.yyyy, HH:mm")) + " - " + ZonedDateTime.parse(item.getString("finishTime"))
-                            .format(DateTimeFormatter.ofPattern("dd.MM.yyyy, HH:mm"))
+                        tv.text =
+                            location.getString("name") + ": " +
+                                    ZonedDateTime.parse(item.getString("startTime"))
+                                        .format(DateTimeFormatter.ofPattern("dd.MM.yyyy, HH:mm:ss")) + " - " +
+                                    ZonedDateTime.parse(item.getString("finishTime"))
+                                        .format(DateTimeFormatter.ofPattern("dd.MM.yyyy, HH:mm:ss"))
                         tv.layoutParams = ll
                         visitsHolder.addView(tv)
                     }
+                }
 
-                    //if (ZonedDateTime.now() < ZonedDateTime.parse(event.getString("finishDate"))) {
-                        runOnUiThread {
-                            val button = Button(applicationContext)
-                            button.text = "Позначити присутність на локації"
+                if (ZonedDateTime.now() < ZonedDateTime.parse(event.getString("finishDate"))) {
+                    var locations = getLocationsOfEvent(event.getString("_id"))
+                    val arrayLoc: MutableList<String> = mutableListOf()
 
+                    for (i in 0 until locations.length()) {
+                        arrayLoc += (JSONObject(locations[i].toString()).getString("name"))
+                    }
+
+                    Log.i("arrayloc", arrayLoc[0])
+
+                    runOnUiThread {
+                        val spinner = Spinner(applicationContext)
+                        spinner.adapter = ArrayAdapter(
+                            applicationContext,
+                            android.R.layout.simple_spinner_item,
+                            arrayLoc
+                        )
+                        var ll = LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.WRAP_CONTENT,
+                            LinearLayout.LayoutParams.WRAP_CONTENT
+                        )
+                        ll.setMargins(0, 20, 0, 0)
+                        spinner.layoutParams = ll
+                        spinner.isClickable = sp.getString("visitStatus", "") != "finish"
+
+                        visitsHolder.addView(spinner)
+
+                        val button = Button(applicationContext)
+                        button.text = "Позначити присутність на локації"
+
+                        button.setOnClickListener() {
+                            if (sp.getString(
+                                    "visitStatus",
+                                    ""
+                                ) == "start" || sp.getString("visitStatus", "") == ""
+                            ) {
+                                sp.edit().putString("visitStatus", "finish").apply()
+                                sp.edit().putString(
+                                    "locId",
+                                    JSONObject(locations[spinner.selectedItemPosition].toString()).getString(
+                                        "_id"
+                                    )
+                                ).apply()
+                                sp.edit()
+                                    .putString("locPos", spinner.selectedItemPosition.toString())
+                                    .apply()
+                                sp.edit().putString(
+                                    "startTime",
+                                    ZonedDateTime.now().toString().split('[')[0]
+                                ).apply()
+                            } else {
+                                sp.edit().putString("visitStatus", "start").apply()
+                                GlobalScope.launch {
+                                    setVisitOnLocation(sp)
+                                }
+                            }
                         }
-                    //}
+
+                        visitsHolder.addView(button)
+                    }
                 }
             }
         } else {
--- app/src/main/java/com/example/lproject_mobile/eventHelper.kt
+++ app/src/main/java/com/example/lproject_mobile/eventHelper.kt
@@ -1,13 +1,16 @@
 package com.example.lproject_mobile
 
 import android.content.SharedPreferences
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
 import io.ktor.client.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.http.content.*
 import org.json.JSONArray
+import java.time.ZonedDateTime
 
 suspend fun getAllEvents(): JSONArray {
     HttpClient().use {
@@ -54,5 +57,34 @@ suspend fun getVisitsByVisitor(eventId: String, sp: SharedPreferences): JSONArra
         }
 
         return events
+    }
+}
+
+suspend fun getLocationsOfEvent(eventId: String): JSONArray {
+    HttpClient().use {
+        val data = it.get<HttpResponse>("http://10.0.2.2:30030/event/$eventId/locations")
+        var locations = JSONArray()
+
+        if (data.status == HttpStatusCode.OK) {
+            locations = JSONArray(data.readText())
+        }
+
+        return locations
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+suspend fun setVisitOnLocation(sp: SharedPreferences) {
+    HttpClient().use {
+        val data = it.post<HttpResponse>("http://10.0.2.2:30030/eventLocation/" + sp.getString("locId", "") + "/visitor") {
+            body = TextContent(
+                "{\"visitorId\": \"${sp.getString("visitorId", "")}\", " +
+                        "\"startTime\": \"${sp.getString("startTime", "")}\"," +
+                        "\"finishTime\": \"${ZonedDateTime.now().toString().split('[')[0]}\"}",
+                ContentType.Application.Json
+            )
+        }
+
+        Log.i("status", data.status.toString())
     }
 }
